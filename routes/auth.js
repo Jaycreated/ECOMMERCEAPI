e0a5a6e7baac70c47d884ecc3fee3c352dd41064
Jay <johnsonjohnadakole@gmail.com>
2022-04-06 17:11:03 +0100

    new

---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+const User = require('../models/User')
+const CryptoJs = require('crypto-js')
+
+//REGISTER
+router.post('/register', async (req,res) =>{
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJs.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+    });
+
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser)
+    }catch (err) {
+        res.status(500).json(err);
+    }
+
+})
+
+//LOGIN
+router.post("/login", async (req, res) =>{
+
+})
+
+module.exports = router
