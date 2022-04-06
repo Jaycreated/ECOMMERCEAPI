660198496fd159dc62808c3fdbf080fe42d2662c
Jay <johnsonjohnadakole@gmail.com>
2022-04-06 13:45:33 +0100

    new

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require('./routes/user')
 
-mongoose.connect('mongodb+srv://Jay:Shunanum7_@cluster0.ehurc.mongodb.net/store?retryWrites=true&w=majority')
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL
+)
     .then(() =>console.log('DBConnection, successful!'))
     .catch((err)=>{console.log(err)});
 
-app.listen(5000, () =>{
+ app.use('/api/user', userRoute)
+
+app.listen(process.env.PORT  || 5000, () =>{
     console.log('backend server is running')
 })
