6de46ef05697e448c56feac179f8c837388a8c8a
Jay <johnsonjohnadakole@gmail.com>
2022-04-06 15:56:47 +0100

    new

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema(
         email: { type: String, required: true, unique: true},
         password: { type: String, required: true, unique: true},
         isAdmin: {
-            type: boolean,
+            type: Boolean,
             default: false,
         },
     },
