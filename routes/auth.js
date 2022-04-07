5877ad8c8dab81f3f3bda21f0ee098f19758b65f
Jay <johnsonjohnadakole@gmail.com>
2022-04-07 10:48:24 +0100

    new

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const User = require('../models/User')
 const CryptoJs = require('crypto-js')
+const jwt = require('jsonwebtoken')
 
 //REGISTER
 router.post('/register', async (req,res) =>{
@@ -22,7 +23,30 @@ router.post('/register', async (req,res) =>{
 
 //LOGIN
 router.post("/login", async (req, res) =>{
+    try{
+    const user = await User.findOne({username: req.body.username});
+    !user && res.status(401).json("wrong credentials!")
 
+    const hashedPassword = CryptoJs.AES.decrypt(user.password, process.env.PASS_SEC);
+    const Originalpassword = hashedPassword.toString( CryptoJs.enc.Utf8 );
+
+    Originalpassword !==req.body.password && res.status(401).json("wrong credentials");
+
+    const accessToken = jwt.sign({
+        id:user._id,
+        isAdmin: user.isAdmin,
+    },
+        process.env.JWT_SEC,
+        {expiresIn:'3d' }
+    );
+
+    const { password, ...others} =user._doc;
+
+        res.status(200).json({...others, accessToken})
+
+  } catch (err){
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
